56dd21b6ceec162a5ee6e8b54ed1ecd8d76dfda4
Andres-Uribe55 <omaruribe0609@gmail.com>
2025-10-30 18:04:35 -0500

    Revert "Feature/omar"

--- Services/Interfaces/IEmailService.cs
+++
@@ -1,9 +0,0 @@
-namespace CrudCloud.api.Services;
-
-public interface IEmailService
-{
-    Task SendEmailVerificationAsync(string toEmail, string userName, string verificationToken);
-    Task SendPasswordResetAsync(string toEmail, string userName, string resetToken);
-    Task SendPlanUpgradeNotificationAsync(string toEmail, string userName, string oldPlan, string newPlan);
-    Task SendWelcomeEmailAsync(string toEmail, string userName);
-}
